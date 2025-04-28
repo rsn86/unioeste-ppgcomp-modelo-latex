abc5ef92dbb3142c79e11f86e8629c7c8328ec0d
Rodrigo Schmidt Nurmberg <rsn86@users.noreply.github.com>
2025-04-28 18:15:10 -0300

    Initial Overleaf Import

---
+++ Codigos/hello.c
@@ -0,0 +1,13 @@
+/**
+ * Arquivo: hello.c
+ * Implementa: Solucao do Problema beecrowd 1000 "Hello
+ * World"
+ */
+
+#include <stdio.h>
+
+int main()
+{
+   printf("Hello World!\n");
+   return 0;
+}
